24e6e837401954f1cc208060d99f0c8c6e395d8e
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-05-14 21:15:48 +0100

    chore: track file changes
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

---
+++ handlers/blogHandler.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gabrielg2020/blog/logger"
+	"github.com/gabrielg2020/blog/services"
+	"github.com/gin-gonic/gin"
+)
+
+func Blog(ctx *gin.Context) {
+	// Load posts
+	posts, err := services.LoadPostData()
+	if err != nil {
+		logger.Error("Failed to load blog post data: ", err)
+		ErrorPage(ctx)
+		return
+	}
+
+	ctx.HTML(http.StatusOK, "blogs.html", gin.H{
+		"Posts": posts,
+	})
+}
